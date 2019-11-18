bdd42d3f5ba3c31ac0e245b00906a72920c516e0
MaikeBezerra <maike_ti@hotmail.com>
2019-11-18 00:42:00 -0300

    Criando View adicionar contato

---
+++ app/src/main/java/com/example/agenda/ContatoActivity.java
@@ -0,0 +1,19 @@
+package com.example.agenda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class ContatoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contato);
+    }
+
+    public void adicionar(View view){}
+
+    public void cancelar(View view){}
+}
